265d35913f7c6efca7909a25a9e7c2bfa5449f4f
Magesh <tkmagesh77@gmail.com>
2017-01-20 09:49:59 +0530

    Incorporating GET and POST requests in app server

--- Day-01/06-AppServer/server.js
+++ Day-01/06-AppServer/server.js
@@ -21,7 +21,7 @@ var server = http.createServer(function(req, res){
 			return;
 		}
 		fs.createReadStream(resourcePath).pipe(res);
-	} else if (resourceObj.pathname === '/calculator'){
+	} else if (resourceObj.pathname === '/calculator' && req.method === 'GET'){
 		var reqData = qs.parse(resourceObj.query),
 			op = reqData.op,
 			n1 = parseInt(reqData.n1,10),
@@ -30,6 +30,22 @@ var server = http.createServer(function(req, res){
 		var result = calculator[op](n1,n2);
 		res.write(result.toString());
 		res.end();
+	} else if (resourceObj.pathname === '/calculator' && req.method === 'POST'){
+		var reqDataRaw = '';
+		req.on('data', function(chunk){
+			reqDataRaw += chunk;
+		});
+		req.on('end', function(){
+			var reqData = qs.parse(reqDataRaw),
+				op = reqData.op,
+				n1 = parseInt(reqData.n1,10),
+				n2 = parseInt(reqData.n2, 10);
+
+			var result = calculator[op](n1,n2);
+			res.write(result.toString());
+			res.end();
+		})
+		
 	} else {
 		res.statusCode = 404;
 		res.end();
